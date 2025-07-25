17ea0a7c02f1233d9485352c91d1d643a72a1c09
Harry Gonzalez <hgonzalez@digitalocean.com>
2025-07-24 22:59:07 -0500

    Redis Controller

--- app/Http/Controllers/RedisInfoController.php
+++ app/Http/Controllers/RedisInfoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 
 class RedisInfoController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RedisInfoController;
+
 
 Route::get('/', function () {return view('welcome');});
 
