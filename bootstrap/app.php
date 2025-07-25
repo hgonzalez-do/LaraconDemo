7791dcc5e10490f5003654dc88522fab7186ad5c
Harry Gonzalez <hgonzalez@digitalocean.com>
2025-07-24 22:01:19 -0500

    Update Trusted Proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,12 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-    $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::do-connecting-ip
-        );
+    $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
